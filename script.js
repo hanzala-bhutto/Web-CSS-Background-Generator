deb6e07bb797318fd115949fac5b9bb6b5e01966
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2022-09-30 21:04:38 +0500

    h3 tag current value changed at runtime

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 var color1 = document.querySelector('.color1');
 var color2 = document.querySelector('.color2');
 var body = document.querySelector("#gradient"); 
+var h3 = document.querySelector("h3");
 
 function setGradient(){
-    console.log(color1.value);
     body.style.background = "linear-gradient(to right,"+color1.value+ ","+color2.value+")";
+    h3.innerHTML = body.style.background;
 }
 
 color1.addEventListener("input",setGradient);
